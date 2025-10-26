9f2b64dc7ae63f3b173d4a7c0cc1df0ba8590981
lepitaaar <leekus1005@gmail.com>
2025-10-26 18:12:22 +0900

    feat: 자동차 클래스 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class Car {
+    String name;
+    int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        this.position++;
+    }
+}
