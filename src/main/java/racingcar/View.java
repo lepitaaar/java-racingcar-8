b043585db78776038774529f2d9be89ef5b51517
lepitaaar <leekus1005@gmail.com>
2025-10-26 18:23:13 +0900

    feat: 뷰 클래스 구현

---
+++ src/main/java/racingcar/View.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import java.util.List;
+
+public class View {
+
+    public static void inputCars() {
+    }
+
+    public static void inputRounds() {
+    }
+
+    public static void printWinner(List<Car> winner) {
+    }
+
+    public static void printCarsPosition(List<Car> cars) {
+    }
+
+}
