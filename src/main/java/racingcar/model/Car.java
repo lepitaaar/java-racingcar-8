3e0bfe7fb53e01346db217de2f7e9559f6defb4a
lepitaaar <leekus1005@gmail.com>
2025-10-27 15:08:38 +0900

    feat: 자동차이름 특수문자 포함 검증 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,12 +1,19 @@
 package racingcar.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Car {
+    private final static String NAME_REGEX = "[!@#$%^&*]";
+    private final static Pattern pattern = Pattern.compile(NAME_REGEX);
+
     private final String name;
     private int position;
     final int MAX_NAME_LENGTH = 5;
 
     public Car(String name) {
         validateNameLength(name);
+        validateNameFormat(name);
         this.name = name;
         this.position = 0;
     }
@@ -25,6 +32,13 @@ public class Car {
 
     private void validateNameLength(String name) {
         if (name.length() > MAX_NAME_LENGTH || name.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNameFormat(String name) {
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.find()) {
             throw new IllegalArgumentException();
         }
     }
