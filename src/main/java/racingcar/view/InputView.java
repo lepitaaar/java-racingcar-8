d9651ce9ad4e1012cd27da808323334c8bf53f89
lepitaaar <leekus1005@gmail.com>
2025-10-27 15:04:11 +0900

    refactor: 자동차 이름 구분자 상수로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,10 +3,12 @@ package racingcar.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
+    private static final String DELIMITER = ",";
+
     public static String[] inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = readLine();
-        return carNames.split(",");
+        return carNames.split(DELIMITER);
     }
 
     public static int inputRounds() {
