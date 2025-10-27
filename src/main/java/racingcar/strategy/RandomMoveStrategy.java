385c7a23481c113a89c8a3f050954ea30c105e99
lepitaaar <leekus1005@gmail.com>
2025-10-27 15:18:41 +0900

    refactor: 랜덤 전략 매직넘버 상수화

--- src/main/java/racingcar/strategy/RandomMoveStrategy.java
+++ src/main/java/racingcar/strategy/RandomMoveStrategy.java
@@ -4,10 +4,12 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMoveStrategy implements MoveStrategy {
     private static final int MOVE_THRESHOLD = 4;
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
 
     @Override
     public boolean isMovable() {
-        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        int randomNumber = Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
         return randomNumber >= MOVE_THRESHOLD;
     }
 }
