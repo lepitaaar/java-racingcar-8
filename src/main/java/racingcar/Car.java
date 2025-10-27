1e09e33bbd66937a051a58095a6c03f5a3045884
lepitaaar <leekus1005@gmail.com>
2025-10-27 11:10:56 +0900

    feat: 자동차의 이름은 5자 이하 검증 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,13 +3,21 @@ package racingcar;
 public class Car {
     String name;
     int position;
+    final int MAX_NAME_LENGTH = 5;
 
     public Car(String name) {
+        validateNameLength(name);
         this.name = name;
         this.position = 0;
     }
 
     public void move() {
         this.position++;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
     }
 }
